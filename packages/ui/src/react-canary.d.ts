08639bed7826270264a7a5cb6156978e3c4a6b21
KairosUnsupervised <281381725+KairosUnsupervised@users.noreply.github.com>
2026-05-28 19:38:52 +0200

    feat(types): Extend `ViewTransitionProps` with new event handlers and class value options

--- packages/ui/src/react-canary.d.ts
+++ packages/ui/src/react-canary.d.ts
@@ -1,10 +1,43 @@
 import type { FC, ReactNode } from "react";
 
 declare module "react" {
-	interface ViewTransitionProps {
-		name?: string;
-		children?: ReactNode;
+	type ViewTransitionClassValue =
+		| "auto"
+		| "none"
+		| string
+		| {
+				[type: string]: "auto" | "none" | string;
+				default?: "auto" | "none" | string;
+		  };
+
+	interface ViewTransitionInstance {
+		old: Element;
+		new: Element;
+		name: string;
+		group: Element;
+		imagePair: Element;
 	}
+
+	type ViewTransitionEventHandler = (
+		instance: ViewTransitionInstance,
+		types: string[],
+	) => () => void;
+
+	interface ViewTransitionProps {
+		children?: ReactNode;
+		name?: string;
+		enter?: ViewTransitionClassValue;
+		exit?: ViewTransitionClassValue;
+		update?: ViewTransitionClassValue;
+		share?: ViewTransitionClassValue;
+		default?: ViewTransitionClassValue;
+		onEnter?: ViewTransitionEventHandler;
+		onExit?: ViewTransitionEventHandler;
+		onUpdate?: ViewTransitionEventHandler;
+		onShare?: ViewTransitionEventHandler;
+	}
+
 	const ViewTransition: FC<ViewTransitionProps>;
+
 	function addTransitionType(type: string): void;
 }
