cde4fd140a138aea18d69753d96bbb90e27270e1
KairosUnsupervised <281381725+KairosUnsupervised@users.noreply.github.com>
2026-06-09 20:54:19 +0200

    fix(ui): Fix pull being possible without any equipment

--- packages/ui/src/context/gacha/library/PullProcess.ts
+++ packages/ui/src/context/gacha/library/PullProcess.ts
@@ -4,49 +4,63 @@ import {makeAutoObservable} from "mobx";
 import {EquipmentInput} from "./EquipmentInput";
 
 export class PullProcess {
-	public equipmentWhitelist = new EquipmentInput();
-	public rarityLuck = new NumberInput({});
-	public floatLuck = new NumberInput({});
-	public revealAmount = new NumberInput({});
-	public pickAmount = new NumberInput({});
-	public visibilityLevel = new NumberInput({default: 1});
+    public equipmentWhitelist = new EquipmentInput();
+    public rarityLuck = new NumberInput({});
+    public floatLuck = new NumberInput({});
+    public revealAmount = new NumberInput({});
+    public pickAmount = new NumberInput({});
+    public visibilityLevel = new NumberInput({default: 1});
 
-	constructor() {
-		makeAutoObservable(this);
-	}
+    constructor() {
+        makeAutoObservable(this);
+    }
 
-	public applyOperation = (operation: AllOperations): void => {
-		switch (operation.field) {
-			case Field.RarityLuck:
-				this.rarityLuck.doOperation(operation);
-				break;
+    public applyOperation = (operation: AllOperations): void => {
+        switch (operation.field) {
+            case Field.RarityLuck:
+                this.rarityLuck.doOperation(operation);
+                break;
 
-			case Field.FloatLuck:
-				this.floatLuck.doOperation(operation);
-				break;
+            case Field.FloatLuck:
+                this.floatLuck.doOperation(operation);
+                break;
 
-			case Field.RevealAmount:
-				this.revealAmount.doOperation(operation);
-				break;
+            case Field.RevealAmount:
+                this.revealAmount.doOperation(operation);
+                break;
 
-			case Field.PickAmount:
-				this.pickAmount.doOperation(operation);
-				break;
+            case Field.PickAmount:
+                this.pickAmount.doOperation(operation);
+                break;
 
-			case Field.VisibilityLevel:
-				this.visibilityLevel.doOperation(operation);
-				break;
+            case Field.VisibilityLevel:
+                this.visibilityLevel.doOperation(operation);
+                break;
 
-			case Field.EquipmentWhitelist:
-				this.equipmentWhitelist.doOperation(operation)
-				break;
-		}
-	};
+            case Field.EquipmentWhitelist:
+                this.equipmentWhitelist.doOperation(operation)
+                break;
+        }
+    };
 
-	/**
-	 * Checks if a pull process is possible, doesn't account for an envelope to be required
-	 */
-	public isPossible = () => {
-		return this.revealAmount.getValue() >= 1 && this.pickAmount.getValue() >= 1;
-	}
+    /**
+     * Checks if a pull process is possible, doesn't account for an envelope to be required
+     */
+    public isPossible = () => {
+
+        if (this.revealAmount.getValue() <= 0) {
+            return false
+        }
+        ;
+
+        if (this.pickAmount.getValue() <= 0) {
+            return false
+        }
+
+        if (this.equipmentWhitelist.getValue().length === 0) {
+            return false;
+        }
+
+        return true;
+    }
 }
