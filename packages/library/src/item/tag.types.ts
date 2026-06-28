0b2281ea6d620f78b2db8fd6bb554273da721e57
KairosUnsupervised <281381725+KairosUnsupervised@users.noreply.github.com>
2026-06-28 20:09:35 +0200

    refactor(library): Remove deprecated tags

--- packages/library/src/item/tag.types.ts
+++ packages/library/src/item/tag.types.ts
@@ -30,57 +30,11 @@ export enum Tag {
 
 	SelfImmolation = "SELF_IMMOLATION", // So that the two SelfImmolation variants exclude each other
 
-	/**
-	 * Skill Modifier Tags
-	 */
+	// Indicates that a modifier gives bonuses to a respective score category of a skill
 	StrengthSkill = "STRENGTH_SKILL",
 	DexteritySkill = "DEXTERITY_SKILL",
 	ConstitutionSkill = "CONSTITUTION_SKILL",
 	IntelligenceSkill = "INTELLIGENCE_SKILL",
 	WisdomSkill = "WISDOM_SKILL",
 	CharismaSkill = "CHARISMA_SKILL",
-
-	/**
-	 * 5E Cursed stats <br/>
-	 * Negative modifiers apply them so positive modifiers affecting the same stat can blacklist them
-	 */
-	StatStrengthCursed = "STAT_STRENGTH_CURSED",
-	StatDexterityCursed = "STAT_DEXTERITY_CURSED",
-	StatConstitutionCursed = "STAT_CONSTITUTION_CURSED",
-	StatIntelligenceCursed = "STAT_INTELLIGENCE_CURSED",
-	StatWisdomCursed = "STAT_WISDOM_CURSED",
-	StatCharismaCursed = "STAT_CHARISMA_CURSED",
-
-	/**
-	 * @DEPRECATED Passive got removed with v2
-	 */
-	StatPassivesCursed = "STAT_PASSIVES_CURSED",
-
-	/**
-	 * 5E Cursed combat effects <br/>
-	 * Negative modifiers apply them so positive modifiers affecting the same stat can blacklist them
-	 * @DEPRECATED Curses got removed with v2
-	 */
-	CombatReactionCursed = "COMBAT_REACTION_CURSED",
-	CombatArmorClassCursed = "COMBAT_ARMOR_CLASS_CURSED",
-	CombatMovementCursed = "COMBAT_MOVEMENT_CURSED",
-	CombatInitiativeCursed = "COMBAT_INITIATIVE_CURSED",
-	CombatDamageTypeChanged = "COMBAT_DAMAGE_TYPE_CHANGED",
-
-	/**
-	 * Tags for artifact generation
-	 * @DEPRECATED Power level and scaling got removed with v2
-	 */
-	ScalingModifier = "SCALING_MODIFIER",
-
-	/**
-	 * Modifier tags
-	 */
-	ModArmorClass = "MOD_ARMOR_CLASS",
-	ModCharismaSkill = "MOD_CHARISMA_SKILL",
-	ModConstitutionSkill = "MOD_CONSTITUTION_SKILL",
-	ModDexteritySkill = "MOD_DEXTERITY_SKILL",
-	ModIntelligenceSkill = "MOD_INTELLIGENCE_SKILL",
-	ModStrengthSkill = "MOD_STRENGTH_SKILL",
-	ModWisdomSkill = "MOD_WISDOM_SKILL",
 }
